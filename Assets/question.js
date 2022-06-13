0d25188c750616ab10e3cca61bc2858950a9072b
Onaysa Ali <anisa.ali.5999385@gmail.com>
2022-06-13 18:19:39 -0400

    save

---
+++ Assets/question.js
@@ -0,0 +1,58 @@
+//creating an array and passing the number, question, options, and answers
+let questions = [
+  {
+      numb: 1,
+      question: "Commonly used data type DO NOt include:",
+      answer: "Alert",
+      options: [
+          "Strings", 
+          "Booleans", 
+          "Alerts",
+         "Numbers"
+     ]
+  },
+  {
+      numb: 2,
+      question: "The condition in an if/else statements is enclosed within _____",
+      answer: "Parentheses",
+      options: [
+          "Quotes", 
+          "Curly brackets", 
+          "Parentheses", 
+          "Square brackets"
+     ]
+  },
+  {
+      numb: 3,
+      question: "Arrays in Javascript can be used to store _____",
+      answer: "All of the above",
+      options: [
+          "Numbers",
+          "Other arrays",
+          "Booleans",
+          "All of the above"
+      ]
+  },
+  {
+      numb: 4,
+      question: "Strings values must be enclosed within _____ when being assigned to variables.",
+      answer: "Quotes",
+      options: [
+          "Common", 
+          "Curly brackets", 
+          "Quotes", 
+          "Parentheses"
+      ]
+  },
+  {
+      numb: 5,
+      question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+      answer: "JavaScript",
+      options: [
+          "JavaScript", 
+          "Terminal", 
+           "For loops",
+           "Console log"
+     ]
+  },
+];
---
+++ Assets/script.js
@@ -0,0 +1,84 @@
+/*
+step 1. displays start page - title and paragraph
+start button -  (triggers the quiz game and displays timer and question page)F0E3FF 481380 DFBAF7
+​
+
+step 2. displays question page and hides start page - timer will start when the start button is clicked from start page, display question and show 4 four answer buttons,  
+When you click one of the answer button, show correct or wrong. . Total remaining is 75 seconds for 5 questions. total = number of Questions * 15 seconds.
+​
+When you get a wrong answer, your time gets deducted by 15 sec off the clock and it needs to show wrong message. If you get it right, no penalty off clock but you need to show Correct message.
+​
+step 3. Once you answer all the questions, you will be presented with the score and input text to enter your initial and a submit button. The timer should stop
+and time left becomes your score. When you click the submit, it store your initial and score in localstorage
+​
+step 4:
+Show a dashboard of all the highscores 
+*/
+
+//var restartEl = document.querySelector(".restart");
+//var questionEl = document.querySelector(".question");
+//var infoBoxEl = document.querySelector(".info_box");
+//var startBtnEl = document.querySelector(".startBtn");
+//function startquiz() {
+//infoBoxEl.classList.add("d-none");
+//restartEl.classList.remove("");
+//startBtnEl.classList.remove("d-none");
+//}
+//restartEl.addEventListener("click", startquiz);
+//startBtnEl.addEventListener("click", startquiz);
+
+const startBtn = document.querySelector(".startBtn button");
+const infoBox = document.querySelector(".infoBox");
+const continu_btn = infoBox.querySelector(".buttons .restart");
+const quizBox = document.querySelector(".quizBox");
+
+//If Start Quiz Button Clicked
+startBtn.onclick = () => {
+    infoBox.classList.add("activeInfo"); //show the info box
+}
+//If Continue Button clicked
+continu_btn.onclick = () => {
+  infoBox.classList.remove("activeInfo"); //hide the info box
+  quizBox.classList.add("activeQuiz"); //show the quiz box
+  showQuestions(4);
+}
+
+//If Continue Button Clicked
+//ContinueBtn.onclick = ()=>{
+// infoBox.classList.remove("d-none"); //hide the info box
+//quizBox.classList.add("d-none"); //show the quiz box
+
+//}
+
+let que_count = 0;
+
+const nextBtn = quizBox.querySelector(".nextBtn");
+
+//If Next Button clicked 
+  nextBtn.onclick = ()=>{
+     if(que_count < questions.length - 1){
+         que_count++;
+         showQuestions(que_count);
+     }else{
+         console.log("Questions complete");
+     }
+    
+
+}
+
+//getting questions and options from array
+function showQuestions(index){
+  const que_text = document.querySelector(".question");
+  const option_list = document.querySelector(".optionList");  
+   let que_tag = '<span>'+ questions[index].numb+"."+ questions[index].question+ '</span>';
+   let option_tag = '<div class="option">'+ questions[index].options[0]+'<span></span></div>'
+                   + '<div class="option">'+ questions[index].options[1]+'<span></span></div>'
+                  + '<div class="option">'+ questions[index].options[2]+'<span></span></div>'
+                   + '<div class="option">'+ questions[index].options[3]+'<span></span></div>';
+   que_text.innerHTML = que_tag;
+  option_list.innerHTML = option_tag;
+  
+}
+
+const bottom_ques_counter = quizBox.querySelector(".totalQue");
+let totalQuesCountTag = '<span><p>'+ +'</p>of<p>5</p>Questions</span>';
